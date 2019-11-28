b3152e22d5b37a53cdb7be10963f464e5d95590f
gonzalo-palummo <gonzalo.palummo@gmail.com>
2019-11-27 23:34:16 -0300

    fix all bugs

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,7 +1,7 @@
 const environment = {
   apiUrl: "https://square-puzzle.herokuapp.com/public/api",
   //apiUrl: "http://localhost:8080/square-puzzle-backend/public/api",
-  publicUrl: "https://res.cloudinary.com/square-puzzle/image/upload/v1"
+  publicUrl: "https://res.cloudinary.com/square-puzzle/image/upload"
   //publicUrl: "http://localhost:8080/square-puzzle-backend/public"
 };
 
