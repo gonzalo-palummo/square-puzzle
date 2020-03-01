fd4154fa86a840a61a875146c7af1f9c6fa6dcac
gonzalo-palummo <gonzalo.palummo@gmail.com>
2020-03-01 13:58:19 -0300

    Link to Da Vinci Added

--- src/components/Advert/Advert.js
+++ src/components/Advert/Advert.js
@@ -1,16 +1,18 @@
-import React from "react";
-import "./Advert.css";
-import { get } from "../../services/MultilingualService";
+import React from 'react';
+import './Advert.css';
+import { get } from '../../services/MultilingualService';
 
 function Advert(props) {
   return (
     <>
       <p className="advertTime">{props.counter}</p>
-      <img
-        src={require("./../../images/advice.jpg")}
-        className="advert"
-        alt={get("advert")}
-      />
+      <a href="https://davinci.edu.ar">
+        <img
+          src={require('./../../images/advice.jpg')}
+          className="advert"
+          alt={get('advert')}
+        />
+      </a>
     </>
   );
 }
